7c98d3569c4f9a93953344ee2f30f5e4f6164ca1
EmolLi <duan.li@mail.mcgill.ca>
2019-07-04 13:54:57 -0400

    api:search

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 mongoose.connect(
@@ -14,13 +17,31 @@ mongoose.connect(
   }
 );
 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+// logger
+app.use((req, res, next) => {
+  console.log(req.method, req.originalUrl);
+  next();
+});
+
+app.use("/api", require("./routes/"));
+app.listen(process.env.PORT || 3050, function() {
+  console.log(`server is listening on port ${process.env.PORT || 3050}!`);
+});
+
 /* insert data into db
 let rawdata = fs.readFileSync("games.json");
 let games = JSON.parse(rawdata);
 
 const Game = require("./models/Game");
 
-Game.collection.insert(games, function(err, docs) {
+Game.insertMany(games, function(err, docs) {
   if (err) {
     return console.error(err);
   } else {
@@ -28,4 +49,4 @@ Game.collection.insert(games, function(err, docs) {
   }
 });
 console.log(games.length);
-*/
+ */
---
+++ routes/index.js
@@ -0,0 +1,70 @@
+const router = require("express").Router();
+const Game = require("../models/Game");
+
+// routes for TESTING views ==================
+// router.get("/", function(req, res) {
+//   res.render("index.ejs");
+// });
+router.post("/search", (req, res) => {
+  const {
+    genre, // array
+    subgenre, // array
+    title, // string
+    pid, // string
+    ratingGte, // number
+    ratingLt, // number
+    rCountGte, // number
+    rCountLt // number
+  } = req.body;
+
+  const query = {};
+
+  let numbers = ["ratingGte", "ratingLt", "rCountLt", "rCountGte"];
+  let arrays = ["genre", "subgenre"];
+  for (let n of numbers) {
+    if (req.body[n] && isNaN(req.body[n])) {
+      res.status(400);
+      res.send(`Error: ${n} should be a number.`);
+      return;
+    }
+  }
+  for (let n of arrays) {
+    if (req.body[n] && !Array.isArray(req.body[n])) {
+      res.status(400);
+      res.send(`Error: ${n} should be an array.`);
+      return;
+    }
+  }
+
+  if (genre && genre.length > 0) query.genre = { $in: genre };
+  if (subgenre && subgenre.length > 0) query.subgenre = { $in: subgenre };
+  if (title) query.title = new RegExp(title, "i");
+  if (pid) query.pid = new RegExp(pid, "i");
+  if (ratingGte) query.rating = { $gte: ratingGte };
+  if (ratingLt)
+    query.rating = query.rating
+      ? { ...query.rating, $lt: ratingLt }
+      : { $lt: ratingLt };
+  if (rCountGte) query.rCount = { $gte: rCountGte };
+  if (rCountLt)
+    query.rCount = query.rCount
+      ? { ...query.rCount, $lt: rCountLt }
+      : { $lt: rCountLt };
+
+  Game.find(query, (err, doc) => {
+    if (err) {
+      res.status(500);
+      console.log(err);
+      res.send(`Error.`);
+    } else res.send(doc);
+  });
+});
+
+router.get("*", function(req, res) {
+  res.send("Hello~");
+});
+
+router.post("*", function(req, res) {
+  res.send("Hello~");
+});
+module.exports = router;
